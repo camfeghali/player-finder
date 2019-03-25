787b24d42a0f740985ede96c33d1a6d7e8c8b1cc
github username <github email address>
2019-03-25 13:51:24 -0400

    form validation and error message done

---
+++ frontend/index.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  console.log("HEY Camille and Dolma are in the DOM!!!!!")
+  const username = document.querySelector('#name')
+  const passwordField = document.querySelector('#password')
+  const submitButton = document.querySelector('#submit-button')
+  const USER_URL = 'http://localhost:3000/users'
+
+  submitButton.addEventListener('click', validate)
+
+
+  function validate(e) {
+    e.preventDefault();
+    fetch (USER_URL)
+    .then (res => res.json())
+    .then (users => validateAndFindUser(users))
+    .then (found => loginHandler(found))
+  }
+
+  function validateAndFindUser(users) {
+    console.log(users)
+    return users.find(function (user) {
+      return (user.username === username.value && user.passwordField === passwordField.value)
+    })
+  }
+
+  function loginHandler(returnedUser){
+    if (returnedUser) {
+      loadFirstPage()
+    }else{
+      showLoginError()
+    }
+  }
+
+  function loadFirstPage(){
+    console.log("first page loaded")
+  }
+
+  function showLoginError(){
+    passwordField.insertAdjacentHTML('afterend', '<p id="error" > Invalid Username or Password </p>')
+  }
+
+
+
+})
